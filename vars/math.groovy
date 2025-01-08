c406c7c535e6c33ca976e308d4158200a1602cac
ggautamaryaa <111658474+ggautamaryaa@users.noreply.github.com>
2025-01-08 11:57:49 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,16 @@
+def sum (a,b)
+{
+  echo "the sum of ${a} and ${b} of ${a+b}"
+}
+def sub (a,b)
+{
+  echo "the subtraction of ${a} and ${b} of ${a-b}"
+}
+def mul (a,b)
+{
+  echo "the multipal of ${a} and ${b} of ${a*b}"
+}
+def dev (a,b)
+{
+  echo "the devision of ${a} and ${b} of ${a/b}"
+}
